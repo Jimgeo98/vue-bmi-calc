b021b4ec5fa2f122e84713030bbaea8255ff6ed8
Dimitris Georgiadis <60574050+geojimas@users.noreply.github.com>
2021-09-14 23:58:45 +0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="page-footer indigo darken-4 animate__animated animate__slideInUp">
       <div class="foo">
         <p>© 2021 Dimitris Georgiadis</p> 
-        <a href="https://github.com/Jimgeo98" target="_blank">Github</a>
+        <a href="https://github.com/geojimas" target="_blank">Github</a>
       </div>
   </footer>
 </template>
